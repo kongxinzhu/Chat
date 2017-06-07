19ab78966b234319249b116642a71425edd38812
Xinzhu Cai <kongxinzhu.s@gmail.com>
2017-06-07 15:09:52 -0700

    user list adapter added user's photo user profile will show photo

--- app/src/main/java/com/marceme/marcefirebasechat/profile/MyProfileActivity.java
+++ app/src/main/java/com/marceme/marcefirebasechat/profile/MyProfileActivity.java
@@ -3,13 +3,19 @@ package com.marceme.marcefirebasechat.profile;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,8 +25,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.marceme.marcefirebasechat.FireChatHelper.ChatHelper;
 import com.marceme.marcefirebasechat.R;
 import com.marceme.marcefirebasechat.login.LogInActivity;
-import com.marceme.marcefirebasechat.model.User;
 import com.marceme.marcefirebasechat.ui.MainActivity;
+import com.squareup.picasso.Picasso;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -32,6 +38,7 @@ public class MyProfileActivity extends Activity {
 //    @BindView(R.id.profile_displayname) EditText mUserDisplayname;
     private EditText mUserDisplayname;
     private EditText mUserStatus;
+    private ImageView mUserPhoto;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -40,7 +47,7 @@ public class MyProfileActivity extends Activity {
     private ChildEventListener mChildEventListener;
     private DatabaseReference mUserRow;
     private String mCurrentUserUid;
-    private String mCurrentUserDisplayname;
+//    private String mCurrentUserDisplayname;
 //    private String mCurrentUserUid;
 //    private String mCurrentUserUid;
 //    private String mCurrentUserUid;
@@ -54,9 +61,6 @@ public class MyProfileActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
 
-
-
-
         bindButterKnife();
         setAuthInstance();
         setUsersDatabase();
@@ -65,34 +69,72 @@ public class MyProfileActivity extends Activity {
         setUserData(mUser);
         getCurrentUserJava();
         mUserDisplayname = (EditText) findViewById(R.id.profile_displayname);
+        mUserStatus = (EditText) findViewById(R.id.profile_status);
+        mUserPhoto = (ImageView) findViewById(R.id.profile_image);
+        updatePhoto();
+
+//          get url from firebase storage
+//        StorageReference load = getImage(id);
+//
+//        load.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//            @Override
+//            public void onSuccess(Uri uri) {
+//                // Got the download URL for 'users/me/profile.png'
+//                // Pass it to Picasso to download, show in ImageView and caching
+//                Picasso.with(context).load(uri.toString()).into(imageView);
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//                // Handle any errors
+//            }
+//        });
+
+
     }
 
 
 
     private void getCurrentUserJava(){
-//        DatabaseReference mUseDisplayname = mUserRefDatabase.child(mCurrentUserUid).child("displayName");
-//        mUseDisplayname.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//
-//                String dn = dataSnapshot.getValue(String.class);
-//                mUserDisplayname.setText(dn);
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//
-        mUserRow = mUserRefDatabase.child(mCurrentUserUid);
-        mUserRow.addValueEventListener(new ValueEventListener() {
+        DatabaseReference mUserDisplaynameReference = mUserRefDatabase.child(mCurrentUserUid).child("displayName");
+        mUserDisplaynameReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                User user = dataSnapshot.getValue(User.class);
-                mUserDisplayname.setText(mUser.getDisplayName());
+                String dn = dataSnapshot.getValue(String.class);
+                mUserDisplayname.setText(dn);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        DatabaseReference mUserStatusReference = mUserRefDatabase.child(mCurrentUserUid).child("status");
+        mUserStatusReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String status = dataSnapshot.getValue(String.class);
+                mUserStatus.setText(status);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        DatabaseReference mUserPhotoReference = mUserRefDatabase.child(mCurrentUserUid).child("photoURL");
+        mUserPhotoReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String url = dataSnapshot.getValue(String.class);
+                Picasso.with(MyProfileActivity.this).load(url).into(mUserPhoto);
 
             }
 
@@ -109,6 +151,28 @@ public class MyProfileActivity extends Activity {
 //        showProgressBarForUsers();
         mAuth.addAuthStateListener(mAuthListener);
     }
+
+
+    public void updatePhoto() {
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mUser.getDisplayName())
+                .setPhotoUri(Uri.parse("https://images.pexels.com/photos/7720/night-animal-dog-pet.jpg?w=940&h=650&auto=compress&cs=tinysrgb"))
+                .build();
+
+        mUser.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User profile updated.");
+                        }
+                    }
+                });
+    }
+
+
+
+
 
 
     private void setUser() {
@@ -173,9 +237,6 @@ public class MyProfileActivity extends Activity {
         dialog.show();
     }
 
-    private void setmUserRow() {
-        mUserRow = mUserRefDatabase.child(mCurrentUserUid);
-    }
 
     private void dismissAlertDialog() {
         dialog.dismiss();
