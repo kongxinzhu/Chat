7f51440f0afd29201140ed1cd4293c100d0eed6f
Xinzhu Cai <kongxinzhu.s@gmail.com>
2017-06-10 21:16:37 -0700

    edit photo with camera and from photo library

--- app/src/main/java/com/marceme/marcefirebasechat/profile/MyProfileActivity.java
+++ app/src/main/java/com/marceme/marcefirebasechat/profile/MyProfileActivity.java
@@ -54,8 +54,6 @@ import pub.devrel.easypermissions.EasyPermissions;
 public class MyProfileActivity extends Activity implements EasyPermissions.PermissionCallbacks {
     private static final String TAG = MyProfileActivity.class.getSimpleName();
 
-
-//    @BindView(R.id.profile_displayname) EditText mUserDisplayname;
     private EditText mUserDisplayname;
     private EditText mUserStatus;
     private ImageView mUserPhoto;
@@ -64,19 +62,13 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference mUserRefDatabase;
     private FirebaseUser mUser;
-    private ChildEventListener mChildEventListener;
-    private DatabaseReference mUserRow;
     private String mCurrentUserUid;
-//    private String mCurrentUserDisplayname;
-//    private String mCurrentUserUid;
-//    private String mCurrentUserUid;
-//    private String mCurrentUserUid;
-//    private String mCurrentUserUid;
-
+    private String mUserStatusContent;
+    private String mUserPhotoDisplaynameContent;
+    private String mUserPhotoURL;
+    private final String NO_CHANGE = "0";
+    private String mUserNewPhotoURL = "0";
     private AlertDialog dialog;
-
-//    @BindView(R.id.progress_bar_users) ProgressBar mProgressBarForUsers;
-//    @BindView(R.id.recycler_view_users) RecyclerView mUsersRecyclerView;
 
     private static final int RC_STORAGE_PERMS = 102;
     private static final int RC_TAKE_PICTURE = 101;
@@ -85,8 +77,6 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
     private static final int RC_SELECT_PICTURE = 103;
     private StorageReference mStorageRef;
     private Uri mFileUri = null;
-    private  Bitmap avatar = null;
-
 
 
     @Override
@@ -101,30 +91,10 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
         mStorageRef = FirebaseStorage.getInstance().getReference();
         setUser();
         setUserData(mUser);
-        getCurrentUserJava();
         mUserDisplayname = (EditText) findViewById(R.id.profile_displayname);
         mUserStatus = (EditText) findViewById(R.id.profile_status);
         mUserPhoto = (ImageView) findViewById(R.id.profile_image);
-        updatePhoto();
-
-//          get url from firebase storage
-//        StorageReference load = getImage(id);
-//
-//        load.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//            @Override
-//            public void onSuccess(Uri uri) {
-//                // Got the download URL for 'users/me/profile.png'
-//                // Pass it to Picasso to download, show in ImageView and caching
-//                Picasso.with(context).load(uri.toString()).into(imageView);
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception exception) {
-//                // Handle any errors
-//            }
-//        });
-
-
+        getCurrentUserJava();
     }
 
 
@@ -134,12 +104,10 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
         mUserDisplaynameReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-                String dn = dataSnapshot.getValue(String.class);
-                mUserDisplayname.setText(dn);
+                mUserPhotoDisplaynameContent = dataSnapshot.getValue(String.class);
+                mUserDisplayname.setText(mUserPhotoDisplaynameContent);
 
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
@@ -151,8 +119,8 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                String status = dataSnapshot.getValue(String.class);
-                mUserStatus.setText(status);
+                mUserStatusContent = dataSnapshot.getValue(String.class);
+                mUserStatus.setText(mUserStatusContent);
 
             }
 
@@ -167,9 +135,10 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                String url = dataSnapshot.getValue(String.class);
-                Picasso.with(MyProfileActivity.this).load(url).into(mUserPhoto);
-
+                mUserPhotoURL = dataSnapshot.getValue(String.class);
+                Picasso.with(MyProfileActivity.this)
+                        .load(mUserPhotoURL)
+                        .into(mUserPhoto);
             }
 
             @Override
@@ -182,31 +151,8 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
     @Override
     public void onStart() {
         super.onStart();
-//        showProgressBarForUsers();
         mAuth.addAuthStateListener(mAuthListener);
     }
-
-
-    public void updatePhoto() {
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(mUser.getDisplayName())
-                .setPhotoUri(Uri.parse("https://images.pexels.com/photos/7720/night-animal-dog-pet.jpg?w=940&h=650&auto=compress&cs=tinysrgb"))
-                .build();
-
-        mUser.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "User profile updated.");
-                        }
-                    }
-                });
-    }
-
-
-
-
 
 
     private void setUser() {
@@ -283,8 +229,9 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
         String mDisplayname = mUserDisplayname.getText().toString();
         mUserRefDatabase.child(mCurrentUserUid).child("status").setValue(mStatus);
         mUserRefDatabase.child(mCurrentUserUid).child("displayName").setValue(mDisplayname);
-
-
+        if(!mUserNewPhotoURL.equals(NO_CHANGE)) {
+            mUserRefDatabase.child(mCurrentUserUid).child("photoURL").setValue(mUserNewPhotoURL);
+        }
     }
 
     private void goToMainActivity() {
@@ -413,7 +360,7 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
         final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
 
         AlertDialog.Builder builder = new AlertDialog.Builder(MyProfileActivity.this);
-        builder.setTitle("Add Photo!");
+        builder.setTitle("Add Photo by : ");
         builder.setItems(options, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int item) {
@@ -463,7 +410,8 @@ public class MyProfileActivity extends Activity implements EasyPermissions.Permi
 
                         // Get the public download URL
                         mDownloadUrl = taskSnapshot.getDownloadUrl();
-                        String temp = mDownloadUrl.toString();
+                        mUserNewPhotoURL = mDownloadUrl.toString();
+                        Picasso.with(MyProfileActivity.this).load(mUserNewPhotoURL).into(mUserPhoto);
                         // [START_EXCLUDE]
                         hideProgressDialog();
 //                        updateUI(mAuth.getCurrentUser());
